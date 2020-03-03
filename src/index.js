e2c7af01d83fc54ca5dfc687a2272c208b59550a
Arsapol <icyzkungz@gmail.com>
2020-03-03 23:57:50 +0700

    remove unsued

--- src/index.js
+++ src/index.js
@@ -1,12 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Square = (props) => {
+    // const [value, setValue] = useState(null)
+    console.log(`Square : ${props.value}`)
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const Board = () => {
+    const [status, setStatus] = useState(false)
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const handleClick = (i) => {
+        // if (squares[i] == null) {
+        //     setStatus(!status)
+
+        //     console.log(`handleClick ${i}`)
+
+        //     let newSquares = [...squares]
+        //     newSquares[i] = status ? 'X' : 'O'
+        //     setSquares(newSquares)
+        // }
+        if (squares[i] != null){
+            return
+        }
+
+        console.log(`handleClick ${i}`)
+        setStatus(!status)
+
+        let newSquares = [...squares]
+        newSquares[i] = status ? 'X' : 'O'
+        setSquares(newSquares)
+    }
+
+    const renderSquare = (i) => {
+        console.log(`renderSquare : ${i} = ${squares[i]}`)
+        return (
+            <Square
+                onClick={() => {
+                    handleClick(i)
+                }}
+                value={squares[i]}
+            />
+        )
+    }
+
+    return (
+        <div>
+            <div className="status">{status ? 'X' : 'O'}'s turn</div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    )
+}
+
+const Game = () => {
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+    );
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
